3c1acd5e64ef9841f6189e3467189bd0f01aed01
san9_zubr <lapinra@gmail.com>
2023-08-06 16:16:05 -0400

    fix

--- 91.decode-ways.py
+++ 91.decode-ways.py
@@ -6,19 +6,19 @@
 
 # A. Iterative solution
 def iterative_solution(s: str) -> int:
-        res = [0] * (len(s) + 2)
-        res[len(s)] = 1
-        res[len(s) + 1] = 0
+    res = [0] * (len(s) + 2)
+    res[len(s)] = 1
+    res[len(s) + 1] = 0
 
-        for k in range(len(s) - 1, -1, -1):
-            if int(s[k]) == 0:
-                res[k] = 0
-            elif k + 1 <= len(s) and int(s[k:k+2]) > 26:
-                res[k] = res[k + 1]
-            else:
-                res[k] = res[k + 1] + res[k + 2]
+    for k in range(len(s) - 1, -1, -1):
+        if int(s[k]) == 0:
+            res[k] = 0
+        elif k + 1 <= len(s) and int(s[k:k+2]) > 26:
+            res[k] = res[k + 1]
+        else:
+            res[k] = res[k + 1] + res[k + 2]
 
-        return res[0]
+    return res[0]
 
 # B. Recursive solution:
 def f(k, s, res):
